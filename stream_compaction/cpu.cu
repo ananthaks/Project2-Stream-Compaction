318efb1b5febbf32b86600b37285b8b59d4a6b25
ananthaks <anantha.aks123@gmail.com>
2018-09-15 17:22:27 -0400

    Added CPU Scan

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -5,6 +5,9 @@
 
 namespace StreamCompaction {
     namespace CPU {
+
+//#define RUN_DEBUG
+
         using StreamCompaction::Common::PerformanceTimer;
         PerformanceTimer& timer()
         {
@@ -12,15 +15,34 @@ namespace StreamCompaction {
 	        return timer;
         }
 
+		void printArray(int n, int *a, bool abridged = false) {
+			printf("    [ ");
+			for (int i = 0; i < n; i++) {
+				if (abridged && i + 2 == 15 && n > 16) {
+					i = n - 2;
+					printf("... ");
+				}
+				printf("%3d ", a[i]);
+			}
+			printf("]\n");
+		}
+
         /**
          * CPU scan (prefix sum).
          * For performance analysis, this is supposed to be a simple for loop.
          * (Optional) For better understanding before starting moving to GPU, you can simulate your GPU scan in this function first.
          */
         void scan(int n, int *odata, const int *idata) {
-	        timer().startCpuTimer();
-            // TODO
-	        timer().endCpuTimer();
+	        //timer().startCpuTimer();
+
+			odata[0] = 0;
+
+			for(int i = 1; i < n; ++i)
+			{
+				odata[i] = odata[i - 1] + idata[i - 1];
+			}
+
+	        //timer().endCpuTimer();
         }
 
         /**
@@ -30,9 +52,18 @@ namespace StreamCompaction {
          */
         int compactWithoutScan(int n, int *odata, const int *idata) {
 	        timer().startCpuTimer();
-            // TODO
+
+			int outIndex = 0;
+			for (int i = 0; i < n; ++i)
+			{
+				if (idata[i] != 0)
+				{
+					odata[outIndex++] = idata[i];
+				}
+			}
+
 	        timer().endCpuTimer();
-            return -1;
+            return (outIndex);
         }
 
         /**
@@ -41,10 +72,47 @@ namespace StreamCompaction {
          * @returns the number of elements remaining after compaction.
          */
         int compactWithScan(int n, int *odata, const int *idata) {
+
+			// Keeping allocations outside of timer
+			auto *tempValidator = new int[n];
+			auto *scanArray = new int[n];
+
 	        timer().startCpuTimer();
-	        // TODO
+
+			// 1. Compute temporary array
+			for (int i = 0; i < n; ++i)
+			{
+				tempValidator[i] = (idata[i] != 0 ? 1 : 0);
+			}
+
+#ifdef RUN_DEBUG
+			printArray(n, tempValidator, true);
+#endif
+
+			// 2. Perform exclusive scan
+			scan(n, scanArray, tempValidator);
+
+#ifdef RUN_DEBUG
+			printArray(n, odata, true);
+#endif
+
+			// 3. Scatter
+			int outIndex = 0;
+			for(int i = 0; i < n; ++i)
+			{
+				if(tempValidator[i] != 0)
+				{
+					outIndex = scanArray[i];
+					odata[outIndex] = idata[i];
+				}
+			}
+
 	        timer().endCpuTimer();
-            return -1;
+
+			delete[] tempValidator;
+			delete[] scanArray;
+
+            return (outIndex + 1);
         }
     }
 }
